64056125ee5a7f118a6f4ce46a7fe8596ec86cb0
monanand <monica86anand@gmail.com>
2019-11-18 10:59:03 +0530

    new mvn proj

---
+++ src/test/java/Test1.java
@@ -0,0 +1,13 @@
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class Test1
+{
+	@Test
+	public void test1()
+	{
+		assertTrue(true);
+	}
+}
