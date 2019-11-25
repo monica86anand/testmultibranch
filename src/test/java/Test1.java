bf3e63e48e0a15e4e3c5599e223b33a9daf8c308
monica86anand <monica86anand@gmail.com>
2019-11-25 18:08:45 +0530

    upd

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,4 +1,4 @@
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.Test;
@@ -9,5 +9,6 @@ public class Test1
 	public void test1()
 	{
 		assertTrue(true);
+		assertFalse(true);
 	}
 }
